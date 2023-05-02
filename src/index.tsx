d4b7390901ecbfe917f8ce3ef3826f2ca6f502e3
Erik Fecher <erik.fecher1@shu.edu>
2023-05-02 13:07:55 -0400

    rebuild

--- src/index.tsx
+++ src/index.tsx
@@ -9,14 +9,15 @@ let qs:queryString.ParsedQuery = queryString.parse(window.location.search);
 
 let resolveDS = '';
 
-if(qs.dataSource === "dev") {
+/*if(qs.dataSource === "dev") {
   resolveDS = "/_cs_apps/data/dev-data.json";
 } else {
   resolveDS = "https://www.shu.edu/_cs_apps/rest/api/ClassroomInformation.cfc?method=getAll";
-}
+}*/
+
 
 //console.log(qs);
 
 ReactDOM.render(
-<App dataSource={resolveDS}/>, 
+<App dataSource={"/_resources/js/_js_apps/classroom-information/ClassroomInformation.json"}/>, 
 document.getElementById('root'));
